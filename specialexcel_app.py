5330e6c3941d1b9732cc42ef19078c69a78fa02e
oyamasnow358 <oyamayuuji596308@gmail.com>
2025-03-07 22:19:44 +0900

    WW

--- specialexcel_app.py
+++ specialexcel_app.py
@@ -349,3 +349,21 @@ def main():
 
 if __name__ == "__main__":
     main()
+                  # **別のWebアプリへのリンク**
+st.markdown("---")  # 区切り線   
+st.markdown("🌎関連Webアプリに移動する")
+st.markdown("[自立活動指導支援内容](https://aspecialeducationapp-6iuvpdfjbflp4wyvykmzey.streamlit.app/)")
+st.markdown("[特別支援教育で使える療法・分析法一覧](https://bunnsekiapppy-6zctfql94fk2x3ghmu5pmx.streamlit.app/)")
+st.markdown("---")  # 区切り線  
+st.markdown("📁教育・心理分析ツール") 
+st.markdown("[応用行動分析](https://abaapppy-k7um2qki5kggexf8qkfxjc.streamlit.app/)")
+st.markdown("[機能的行動評価分析](https://kinoukoudou-ptfpnkq3uqgaorabcyzgf2.streamlit.app/)") 
+st.markdown("---")  # 区切り線
+st.markdown("📁統計学分析ツール") 
+st.markdown("[相関分析ツール](https://soukan-jlhkdhkradbnxssy29aqte.streamlit.app/)")
+st.markdown("[多変量回帰分析](https://kaikiapp-tjtcczfvlg2pyhd9bjxwom.streamlit.app/)")
+st.markdown("[t検定](https://tkentei-flhmnqnq6dti6oyy9xnktr.streamlit.app/)")
+st.markdown("[ロジスティック回帰分析ツール](https://rojisthik-buklkg5zeh6oj2gno746ix.streamlit.app/)")
+st.markdown("[ノンパラメトリック統計分析ツール](https://nonparametoric-nkk2awu6yv9xutzrjmrsxv.streamlit.app/)")
+st.markdown("---")  # 区切り線
+st.write("""※ それぞれのアプリに記載してある内容、分析ツールのデータや図、表を外部に出す物（研究など）に使用する場合は小山までご相談ください。無断での転記・利用を禁じます。""")
